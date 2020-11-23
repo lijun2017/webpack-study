46d0b30dcf013b1385b212ece40c813df5e9b8e0
lijun <jun.li@hosjoy.com>
2020-11-23 22:21:08 +0800

    压缩css

---
+++ 11压缩css/webpack.config.js
@@ -0,0 +1,59 @@
+
+const path = require('path')
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+// 设置nodejs环境变量
+// process.env.NODE_ENV = 'development'
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          MiniCssExtractPlugin.loader, // 单独生产 css 文件
+          'css-loader', // 将 css 编程js
+          { // css兼容性处理
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                // postcss
+                require('postcss-preset-env')()
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/built.css'
+    }),
+    // 压缩css
+    new OptimizeCssAssetsWebpackPlugin()
+  ],
+
+  // 模式
+  mode: 'development', // development product
+  devServer: {
+    contentBase: resolve(__dirname, 'build'),
+    compress: true, // 启动gzip压缩
+    port: 3001, // 服务器端口
+    open: true // 自动打开浏览器
+  }
+}
