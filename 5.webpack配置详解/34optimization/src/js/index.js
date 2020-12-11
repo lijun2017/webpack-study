477ec292095b45f06fbecf1b18527a25ba2ef46c
lijun <jun.li@hosjoy.com>
2020-12-11 16:34:48 +0800

    minimizer

--- 5.webpack配置详解/34optimization/src/js/index.js
+++ 5.webpack配置详解/34optimization/src/js/index.js
@@ -1,2 +1,6 @@
 import '$css/index.css'
 console.log('index.js被加载了')
+
+import(/* webpackChunkName: 'a' */'./a').then(({add}) => {
+    console.log(add(1, 2))
+})
--- 5.webpack配置详解/34optimization/webpack.config.js
+++ 5.webpack配置详解/34optimization/webpack.config.js
@@ -2,15 +2,16 @@
 const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: './src/js/index.js',
 
   output: {
-    filename: 'built.js',
-    path: path.resolve(__dirname, 'build')
+    filename: 'js/[name].[contenthash:10].js',
+    path: path.resolve(__dirname, 'build'),
+    chunkFilename: 'js/[name].[contenthash:10].js'
   },
-
   module: {
     rules: [
       {
@@ -41,6 +42,9 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all',
+
+      // 以下配置为默认配置
+      /* 
       minSize: 30 * 1024, // 分割的chunk最小为30kb
       maxSize: 0, // 最大没有限制
       minChunks: 1, // 要提取的chunks最少被引用1次
@@ -49,10 +53,37 @@ module.exports = {
       automaticNamedelimiter: '~', // 名称链接符
       name: true, // 可以使用命名规则
       cacheGroups: { // 分割chunk的组
+        // node_modules文件会被打包到vendors组的chunks中。 --> vendors~xxx.js
+        // 满足上面的公共规则则，如：大小超过30kb，至少被引用一次
         vendors: {
-          test: []
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          // 要提取的chunk最少被引用2次
+          minChunks: 2,
+          priority: -20,
+          // 如果当前要打包的模块，和之前已经被提取的模块是同一个，就会复用，二不是重新打包模块
+          reuseExistingChunk: true
         }
       }
-    }
+     */
+    },
+    // 将当亲模块记录其他模块的hash单独打包为一个文件 runtime
+    // 解决：修改a文件导致b文件的contenthash变化
+    runtimeChunk: {
+      name: (entrypoint) => `runtime-${entrypoint.name}`
+    },
+    minimizer: [
+      // 配置生产环境的压缩方法：js和css
+      new TerserWebpackPlugin({
+        cache: true, // 开启缓存
+        parallel: true, // 开启多进程打包
+        sourceMap: true, // 如果在生产环境中使用 source-maps，必须设置为 true
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        }
+      })
+    ]
   }
 }
